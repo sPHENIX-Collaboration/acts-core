ed90e7f4fb442e9165638ea1f6de35af1da05737
Fabian Klimpel <fklimpel@cern.ch>
2019-08-16 16:09:43 +0200

    Compiling version

--- Tests/Core/Propagator/KalmanExtrapolatorTests.cpp
+++ Tests/Core/Propagator/KalmanExtrapolatorTests.cpp
@@ -50,6 +50,7 @@ struct StepWiseActor {
     std::vector<Jacobian> jacobians = {};
     std::vector<double> paths = {};
 
+    //~ Jacobian fullJacobian = Jacobian::Identity();
     double fullPath = 0.;
 
     bool finalized = false;
